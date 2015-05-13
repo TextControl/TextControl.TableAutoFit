51fd7ea8f16985190de11690ba04309ee5dd007f
Gunnar Giffey <gunnar@textcontrol.com>
2015-05-13 11:33:37 +0200

    Added content.

---
+++ Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace tx_table_autofit
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textControl1 = new TXTextControl.TextControl();
+            this.buttonBar1 = new TXTextControl.ButtonBar();
+            this.rulerBar2 = new TXTextControl.RulerBar();
+            this.statusBar1 = new TXTextControl.StatusBar();
+            this.rulerBar1 = new TXTextControl.RulerBar();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.tableToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.insertToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.autoFitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.autoSizeToPageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.combineToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textControl1
+            // 
+            this.textControl1.ButtonBar = this.buttonBar1;
+            this.textControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textControl1.Font = new System.Drawing.Font("Arial", 10F);
+            this.textControl1.Location = new System.Drawing.Point(25, 77);
+            this.textControl1.Name = "textControl1";
+            this.textControl1.RulerBar = this.rulerBar2;
+            this.textControl1.Size = new System.Drawing.Size(989, 431);
+            this.textControl1.StatusBar = this.statusBar1;
+            this.textControl1.TabIndex = 0;
+            this.textControl1.VerticalRulerBar = this.rulerBar1;
+            // 
+            // buttonBar1
+            // 
+            this.buttonBar1.BackColor = System.Drawing.SystemColors.Control;
+            this.buttonBar1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonBar1.Location = new System.Drawing.Point(0, 24);
+            this.buttonBar1.Name = "buttonBar1";
+            this.buttonBar1.Size = new System.Drawing.Size(1014, 28);
+            this.buttonBar1.TabIndex = 1;
+            this.buttonBar1.Text = "buttonBar1";
+            // 
+            // rulerBar2
+            // 
+            this.rulerBar2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.rulerBar2.Location = new System.Drawing.Point(0, 52);
+            this.rulerBar2.Name = "rulerBar2";
+            this.rulerBar2.Size = new System.Drawing.Size(1014, 25);
+            this.rulerBar2.TabIndex = 4;
+            this.rulerBar2.Text = "rulerBar2";
+            // 
+            // statusBar1
+            // 
+            this.statusBar1.BackColor = System.Drawing.SystemColors.Control;
+            this.statusBar1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.statusBar1.Location = new System.Drawing.Point(0, 508);
+            this.statusBar1.Name = "statusBar1";
+            this.statusBar1.Size = new System.Drawing.Size(1014, 22);
+            this.statusBar1.TabIndex = 2;
+            // 
+            // rulerBar1
+            // 
+            this.rulerBar1.Alignment = TXTextControl.RulerBarAlignment.Left;
+            this.rulerBar1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.rulerBar1.Location = new System.Drawing.Point(0, 77);
+            this.rulerBar1.Name = "rulerBar1";
+            this.rulerBar1.Size = new System.Drawing.Size(25, 431);
+            this.rulerBar1.TabIndex = 3;
+            this.rulerBar1.Text = "rulerBar1";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tableToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1014, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // tableToolStripMenuItem
+            // 
+            this.tableToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.insertToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.autoFitToolStripMenuItem,
+            this.autoSizeToPageToolStripMenuItem,
+            this.combineToolStripMenuItem});
+            this.tableToolStripMenuItem.Name = "tableToolStripMenuItem";
+            this.tableToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.tableToolStripMenuItem.Text = "Table";
+            this.tableToolStripMenuItem.DropDownOpening += new System.EventHandler(this.tableToolStripMenuItem_DropDownOpening);
+            // 
+            // insertToolStripMenuItem
+            // 
+            this.insertToolStripMenuItem.Name = "insertToolStripMenuItem";
+            this.insertToolStripMenuItem.Size = new System.Drawing.Size(163, 22);
+            this.insertToolStripMenuItem.Text = "Insert";
+            this.insertToolStripMenuItem.Click += new System.EventHandler(this.insertToolStripMenuItem_Click);
+            // 
+            // autoFitToolStripMenuItem
+            // 
+            this.autoFitToolStripMenuItem.Name = "autoFitToolStripMenuItem";
+            this.autoFitToolStripMenuItem.Size = new System.Drawing.Size(163, 22);
+            this.autoFitToolStripMenuItem.Text = "AutoSize";
+            this.autoFitToolStripMenuItem.Click += new System.EventHandler(this.autoFitToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(160, 6);
+            // 
+            // autoSizeToPageToolStripMenuItem
+            // 
+            this.autoSizeToPageToolStripMenuItem.Name = "autoSizeToPageToolStripMenuItem";
+            this.autoSizeToPageToolStripMenuItem.Size = new System.Drawing.Size(163, 22);
+            this.autoSizeToPageToolStripMenuItem.Text = "AutoSize to page";
+            this.autoSizeToPageToolStripMenuItem.Click += new System.EventHandler(this.autoSizeToPageToolStripMenuItem_Click);
+            // 
+            // combineToolStripMenuItem
+            // 
+            this.combineToolStripMenuItem.Name = "combineToolStripMenuItem";
+            this.combineToolStripMenuItem.Size = new System.Drawing.Size(163, 22);
+            this.combineToolStripMenuItem.Text = "Combine";
+            this.combineToolStripMenuItem.Click += new System.EventHandler(this.combineToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1014, 530);
+            this.Controls.Add(this.textControl1);
+            this.Controls.Add(this.rulerBar1);
+            this.Controls.Add(this.rulerBar2);
+            this.Controls.Add(this.buttonBar1);
+            this.Controls.Add(this.statusBar1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "TX Text Control Sample: Table Auto Fit";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TXTextControl.TextControl textControl1;
+        private TXTextControl.ButtonBar buttonBar1;
+        private TXTextControl.RulerBar rulerBar2;
+        private TXTextControl.StatusBar statusBar1;
+        private TXTextControl.RulerBar rulerBar1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem tableToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem insertToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem autoFitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem autoSizeToPageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem combineToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using TXTextControl;
+
+namespace tx_table_autofit
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void insertToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textControl1.Tables.Add();
+        }
+
+        private void tableToolStripMenuItem_DropDownOpening(object sender, EventArgs e)
+        {
+            if (textControl1.Tables.GetItem() != null)
+                autoFitToolStripMenuItem.Enabled = true;
+            else
+                autoFitToolStripMenuItem.Enabled = false;
+        }
+
+        private void autoFitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textControl1.Tables.GetItem().AutoSize(textControl1);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textControl1.Load("tables.rtf", StreamType.RichTextFormat);
+        }
+
+        private void autoSizeToPageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textControl1.Tables.GetItem().SpreadToPage(textControl1);
+        }
+
+        private void combineToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textControl1.Tables.GetItem().SpreadToPage(textControl1);
+            textControl1.Tables.GetItem().AutoSize(textControl1);
+        }
+    }
+}
